b5faacdbd8099165d0af6a47a4cfc66c093b925c
Jamison Dance <jergason@gmail.com>
2010-09-14 17:01:34 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pocketbook::Application.config.secret_token = 'c9f6b321b77ab66e50da8e5375df0a2b5306d79f947ee1527ad7870ad4cfc9308dea07630404cd31a0628b9512c0e41f867aab6453b27aa3228bccd895fd6199'
